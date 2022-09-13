2ff64efdf6eb31d07a054864357712096850b3b3
link2x <link2x@users.noreply.github.com>
2022-09-13 14:56:08 -0400

    FCP optimization

--- components/header.js
+++ components/header.js
@@ -5,15 +5,15 @@ import logoImage from '../images/ELSEIFDraft.png'
 
 class Logo extends Component {
   render() {
-    return  <div className="one-half column showcenter u-max-full-width">
-              <Image className="resize_fit_center showcenter" src={logoImage} width='200%' height='100%' layout='responsive' objectFit='contain' alt="Logo" />
+    return  <div className="one-half column showcenter u-max-full-width" style={{marginTop: '1.75em', marginBottom: '2em'}}>
+              <Image className="resize_fit_center showcenter" src={logoImage} width={882} height={216} layout='responsive' objectFit='contain' alt="Logo" placeholder='blur' />
             </div>
   }
 }
 
 class Header extends Component {
   render() {
-    return  <Row>
+    return  <Row pad>
               <Logo />
               <div className="one-half column bio pad">
                 <h2>
