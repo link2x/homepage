64c357ada1d521b810c04dfb0627cd8351eccded
link2x <link2x@users.noreply.github.com>
2023-11-04 23:10:34 -0400

    Random title changes

--- functions/randomtitle.js
+++ functions/randomtitle.js
@@ -2,28 +2,22 @@ export function randomTitle() {
 
   let randomTitles = [
     "the islands awaken for her alone",
-    "Where's my cake?",
     "I lost the game.",
-    "Hooray for random sites!",
     "What's that? Something new from me? Never!",
     "Meh.",
     "Oh cool!",
     ":D",
-    "Well isn't that something...",
-    "Ha; I have magic!",
     "Ayo!",
     "New song! Oh look, there's another! And another!",
-    "*complain complain complain*",
     ":|",
     ">:D",
     "O.o",
     ">.<'",
     "Random title dance! *raves*",
     "Some random girl that thinks she's cool.",
-    "Eden Simmons",
+    "Eden",
     "That musician girl.",
-    "Sparsely bringing you music since 2006.",
-    "Isn't it grand?",
+    "Bringing you music since 2006.",
     "Wow!",
     "Releasing an Able-TON of music.",
     "Awesomeness!",
@@ -31,7 +25,8 @@ export function randomTitle() {
     "I will not once give in.",
     "And in the end of night...",
     "Tonight we're on fire.",
-    "1,000 Years"
+    "1,000 Years",
+    "RE:BOOT INITIATED"
   ];
 
   let randomNumber = Math.floor(Math.random() * (randomTitles.length - 1));
